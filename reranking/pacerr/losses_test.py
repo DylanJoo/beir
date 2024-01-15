51c34b06d18ba70f68c773dc93da8c65a819f765
Your Name <you@example.com>
2024-01-15 09:52:19 +0800

    [ascs] update ce methods

--- reranking/pacerr/losses_test.py
+++ reranking/pacerr/losses_test.py
@@ -5,12 +5,38 @@ from torch.nn import functional as F
 from torch.nn import MarginRankingLoss
 from torch.nn import CrossEntropyLoss
 
-"""
-PCLCELoss:
-    - Passage-centric LCE (a positive query with multiple negative queries)
-Groupwuse Hinge Loss V1: 
-    - Dilated hinge loss with stride 
-"""
+class LocalizedCELoss(nn.Module):
+
+    def __init__(self, 
+                 examples_per_group: int = 1, 
+                 margin: float = 1, 
+                 stride: int = 1,    # the size between selected positions
+                 dilation: int = 1,  # the position of the paired negative 
+                 reduction: str = 'mean',
+                 use_in_batch: bool = False):
+        super().__init__()
+        self.examples_per_group = examples_per_group
+        self.loss_fct = MarginRankingLoss(
+                margin=margin, 
+                reduction=reduction
+        )
+        self.activation = nn.Sigmoid()
+        self.stride = stride
+        self.dilation = dilation
+        self.sample_indices = list(
+                range(0, examples_per_group-dilation, stride)
+        )
+
+    def forward(self, logits, labels):
+        loss = 0
+        logits = self.activation(logits)
+        logits = logits.view(-1, self.examples_per_group)
+        targets = torch.ones(logits.size(0)).to(logits.device)
+        for idx in self.sample_indices:
+            logits_positive = logits[:, idx]
+            logits_negative = logits[:, (idx+self.dilation)]
+            loss += self.loss_fct(logits_positive, logits_negative, targets)
+        return loss / len(self.sample_indices)
 
 class GroupwiseHingeLossV1(nn.Module):
 
